944da24220ad76a7e4afeac0e359f6c3246486d2
SamuelTurner <sam@samuel-turner.co.uk>
2014-01-01 14:09:31 +0000

    Refactored the demo to use dependancies as sub-modules

--- assets/js/main.js
+++
@@ -1,2 +0,0 @@
-/* @import(clingy-footer/clingy-footer-1.2.0.js) */
-/* @import(samuelturnerui/samuelturnerui-1.0.0.js) */
---
+++ assets/main.js
@@ -0,0 +1,2 @@
+/* @import(Clingy-Footer/clingy-footer-1.2.0.js) */
+/* @import(SamuelTurnerUI/js/samuelturnerui-1.0.0.js) */
